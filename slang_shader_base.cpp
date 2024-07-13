c788ea25d41e7e8dba1796c45a53f678aa8b12e7
Louis Child <thelouischild@gmail.com>
2024-07-13 21:10:53 +0100

    Fixed linker errors

--- slang_shader_base.cpp
+++ slang_shader_base.cpp
@@ -1,14 +1,14 @@
 #include "slang_shader_base.h"
 
-// #include "slang/slang.h"
-// #include "slang/slang-com-ptr.h"
+#include "slang/slang.h"
+#include "slang/slang-com-ptr.h"
 
-// using namespace slang;
+using namespace slang;
 
 SlangShaderBase::SlangShaderBase() {
 	m_spirv_data  = PackedByteArray();
 	m_entry_point = "";
 
-	// Slang::ComPtr<IGlobalSession> globalSession;
-	// createGlobalSession(globalSession.writeRef());
+	Slang::ComPtr<IGlobalSession> globalSession;
+	createGlobalSession(globalSession.writeRef());
 }
